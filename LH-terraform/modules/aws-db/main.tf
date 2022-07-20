2b66f5fe19b7af2f0d4e458fc9404b9e83c7402c
kath <katherine.linw@gmail.com>
2022-07-20 21:25:20 +1000

    db tf

---
+++ LH-terraform/modules/aws-db/main.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  region = "ap-southeast-2"
+  profile = "default"
+  access_key = var.your_access_key
+  secret_key = var.your_secret_key
+}
+
+resource "aws_dynamodb_table" "LH-dynamodb" {
+    name = "LH-dynamodb"
+    billing_mode   = "PROVISIONED"
+    read_capacity  = 20
+    write_capacity = 20
+
+    hash_key="UserId"
+
+    attribute{
+      name = "UserId"
+      type = "S"
+    }
+}
+
