bbe90d526185310c99591311ac17ce3094ede382
kath <katherine.linw@gmail.com>
2022-07-22 22:02:05 +1000

    *

--- LH-client/lh-client/craco.config.js
+++ LH-client/lh-client/craco.config.js
@@ -1,3 +1,17 @@
+const CracoLessPlugin = require('craco-less');
+
 module.exports = {
-    // ...
-  };
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            modifyVars: { '@primary-color': '#088F8F' },
+            javascriptEnabled: true,
+          },
+        },
+      },
+    },
+  ],
+};
